5c1fdd9b0e9805fcef4f8c2cce149d5f36eb1afa
Maximilien Monteil <maximilienmonteil@gmail.com>
2020-01-31 14:07:21 +0200

    Make all icon components functional

--- src/components/IconBase.vue
+++ src/components/IconBase.vue
@@ -1,30 +1,47 @@
-<template>
+<template functional>
   <svg xmlns="http://www.w3.org/2000/svg"
-    :width="width"
-    :height="height"
+    :width="props.size"
+    :height="props.size"
     viewBox="0 0 18 18"
-    :aria-labelledby="iconName"
+    :aria-labelledby="props.iconName"
     role="presentation"
+    :class="[
+      data.class,
+      data.staticClass
+    ]"
+    :style="[
+      data.style,
+      data.staticStyle
+    ]"
   >
-    <title :id="iconName" lang="en">{{iconName}} icon</title>
-    <g :fill="iconColor">
-      <slot />
+    <title :id="props.iconName" lang="en">{{ props.iconName.replace(/-/g, ' ') }} icon</title>
+    <g :fill="props.iconColor">
+      <component :is="injections.components[props.iconName]" />
     </g>
   </svg>
 </template>
 
 <script>
 export default {
+  inject: {
+    components: {
+      default: {
+        'box': () => import('./icons/IconBox'),
+        'calendar': () => import('./icons/IconCalendar'),
+        'envelope': () => import('./icons/IconEnvelope'),
+        'garbage': () => import('./icons/IconGarbage'),
+        'image': () => import('./icons/IconImage'),
+        'moon': () => import('./icons/IconMoon'),
+        'write': () => import('./icons/IconWrite')
+      }
+    }
+  },
   props: {
     iconName: {
       type: String,
       default: 'box'
     },
-    width: {
-      type: [Number, String],
-      default: 18
-    },
-    height: {
+    size: {
       type: [Number, String],
       default: 18
     },
